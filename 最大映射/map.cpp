5e589fbb6476c8ac070f6b8881a7362d738a6e69
Junjie Li <goals.lee@qq.com>
2016-05-18 20:30:20 +0800

    update

--- 最大映射/map.cpp
+++ 最大映射/map.cpp
@@ -28,16 +28,18 @@ using namespace std;
 
 int main()
 {
-    vector<pair<long long,char>> sum = {{0,'A'},{0,'B'},{0,'C'},{0,'D'},{0,'E'},{0,'F'},{0,'G'},{0,'H'},{0,'I'},{0,'J'}};
+    vector<pair<long long,char>> sum {{0,'A'},{0,'B'},{0,'C'},{0,'D'},{0,'E'},{0,'F'},{0,'G'},{0,'H'},{0,'I'},{0,'J'}};
+    vector<char> mark;
     int n;
     long long ret=0;
     cin>>n;
-    vector<char> mark;
+
     while(n--){
+        long long weight=1;
         string input;
         cin>>input;
         mark.push_back(input[0]);
-        long long weight=1;
+
         for(auto iter=input.rbegin();iter!=input.rend();++iter){
             sum[*iter-'A'].first+=weight;
             weight*=10;
